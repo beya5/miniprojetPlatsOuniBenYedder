2496595adbabdc88189dd5b1765cf8c182548a32
beya <beyabenyedder33@gmail.com>
2024-11-17 23:53:09 +0100

    half login

--- projects/back-office/src/app/login/login.component.ts
+++ projects/back-office/src/app/login/login.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import{FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  fb:FormBuilder= inject(FormBuilder);
+  subForm!:FormGroup;
+  private router:Router=inject(Router);
+  private readonly donnees={admin:'admin',mdp:'123123.'};
 
+  ngOnInit(): void {
+    this.subForm=this.fb.nonNullable.group(
+      {
+        admin:['',[Validators.required]],
+        mdp:['',[Validators.required]]
+      }
+    )
+  }
+  get admin(){
+    return this.subForm.get('admin');
+  }
+  get mdp(){
+    return this.subForm.get('mdp');
+  }
+  public isOblig(controlName:string){
+    const control=this.subForm.get(controlName);
+    return control?.errors?.['required'] && control?.touched;
+  }
+  onResetF(){
+    this.subForm.reset();
+  }
+  onSubmit(){
+    if(this.subForm.valid){
+      const adm=this.admin?.value;
+      const mdp=this.mdp?.value;
+      if(adm === this.donnees.admin && mdp == this.donnees.mdp){
+        this.router.navigate(['/']);
+      }else{
+        alert('Identifiant ou mot de passe invalide');
+      }
+    }
+  }
 }
